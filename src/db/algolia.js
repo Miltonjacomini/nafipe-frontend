c0b5175d27e127a39bd14e79ea168bd65ae3242c
Milton Giacomini Neto <milton.jneto@mercadolivre.com>
2024-10-01 21:41:57 -0300

    Update Algolia client to get variables from .env

--- src/db/algolia.js
+++ src/db/algolia.js
@@ -1,5 +1,8 @@
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
 
-
+const searchClient = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
 
 export default searchClient;
